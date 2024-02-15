3541cc101a26a679cd72162f1be2e4b6d4b6e3a4
Pandora <imcathalat@gmail.com>
2024-02-15 17:17:29 -0300

    sign in pythonando

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='user_login')
+    path('login/', views.user_login, name='user_login'),
+    path('sign_in/', views.user_sign, name="user_sign")
 ] 
--- users/views.py
+++ users/views.py
@@ -1,27 +1,43 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth.models import User
+
+def user_sign(request):
+    if request.method == 'GET':
+        return render(request, 'registration/sign_in.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('Já existe um usuário com esse user')
+        
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        return HttpResponse("usuario cadastrado com sucesso")
+
 
 def user_login(request):
-    print("aaaaaaaaaaaaaa")
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        print(user)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        return HttpResponse(username)
 
-        if user is not None:
-            login(request, user)
-            print('user')
-            return redirect('home')
-            
-        else:
-            messages.success(request, ('There Was An Error Logging In, Try Again.'))
-            print('primeiro else')
-            return redirect('login')
-    else:
-        print('caiu no segundo else')
-        return render(request, 'registration/login.html')
+    # if user is not None:
+    #     login(request, user)
+    #     print('user')
+    #     return redirect('home')
+        
+    # else:
+    #     messages.success(request, ('There Was An Error Logging In, Try Again.'))
+    #     print('primeiro else')
+    
         
 
 
